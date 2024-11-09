c20cbcd8e98fd23debad95f256bc4598042f28c2
CamachoGabriela <412016@tecnicatura.frc.utn.edu.ar>
2024-11-09 12:13:49 -0300

    limpiar_datos()

--- FrontEnd/JS/consultaButaca.js
+++ FrontEnd/JS/consultaButaca.js
@@ -1,13 +1,13 @@
 document.getElementById('boton_consultar').addEventListener('click', async function() {
-    datos_butaca(); //limpiar datos de la butaca
+    datos_butaca(null); //limpiar datos de la butaca
     const $div = document.getElementById('butacas_cargadas');
     const $selectP = document.getElementById('pelicula_select');
     const $selectF =  document.getElementById('funcion_select');
     const funcion =  $selectF.options[$selectF.selectedIndex].value;
-    const $ocultar = document.getElementById('hide');
+    
     $div.innerHTML = '';
     let images = '';
-    $ocultar.style.display = 'none';
+   
     $div.style.display = 'flex'; 
     try
     {
@@ -25,15 +25,13 @@ document.getElementById('boton_consultar').addEventListener('click', async funct
             for (let j = 0; j < 10; j++) {
                 let butaca_img =  'Libre.png'
                 let clase = 'butaca' //cambia el comportamiento del css
-                //asigna la funcion para mostrar los datos
-                let mostrar_datos = `datos_butaca('${data[cont].idButaca},${fpartes[0]},${$selectP.options[$selectP.selectedIndex].textContent},${fpartes[2]}');`
                 //valida si es una butaca reservable
-                if(data[cont].estado == 1 ){butaca_img =  'Ocupado.png';clase = 'butaca_ocupada';mostrar_datos = ''}
+                if(data[cont].estado == 1 ){butaca_img =  'Ocupado.png';clase = 'butaca_ocupada';}
                 //si es una butaca pendiente le pone la imagen correspondiente
-                if(data[cont].estado == 2){butaca_img = 'reservado.png';clase = 'butaca_ocupada';mostrar_datos = ''}
+                if(data[cont].estado == 2){butaca_img = 'reservado.png';clase = 'butaca_ocupada';}
                 images += //creando las butacas
                 `
-                <div class="${clase}" onclick="${mostrar_datos}toggleSelected(this);"data-id="${data[cont].idButaca}">
+                <div class="butaca" data-id="${data[cont].idButaca}" data-sala="${fpartes[0]}" data-pelicula="${$selectP.options[$selectP.selectedIndex].textContent}" data-funcion="${fpartes[2]}" data-precio="${fpartes[3]}" onclick="handleButacaClick(this)">
                     <img class="butaca_img" src="/IMAGES/${butaca_img}" alt="butaca">
                     <p class="align-middle mb-2" id="" style="font-size: 0.6em;">${data[cont].nroButaca}</p>
                 </div>
@@ -48,7 +46,6 @@ document.getElementById('boton_consultar').addEventListener('click', async funct
 })
 
 function toggleSelected(element) {
-    var selectedElement = document.querySelector('.butaca.selected');
     const img = element.querySelector('.butaca_img');
     const isSelected = element.classList.toggle('selected');
     if(isSelected) {
@@ -56,14 +53,27 @@ function toggleSelected(element) {
         img.src = '/IMAGES/Seleccionado.png';
     } else {
         img.src = element.getAttribute('data-original-src');
-        datos_butaca();
-    }
-    if (selectedElement && selectedElement !== element) {
-        const selectedImg = selectedElement.querySelector('.butaca_img');
-        selectedImg.src = selectedElement.getAttribute('data-original-src');
-        selectedElement.classList.remove('selected');
     }
 }
+function handleButacaClick(element) {
+    const id = element.getAttribute('data-id');
+    const sala = element.getAttribute('data-sala');
+    const pelicula = element.getAttribute('data-pelicula');
+    const funcion = element.getAttribute('data-funcion');
+    const precio = element.getAttribute('data-precio');
+
+    // Llamar a toggleSelected para manejar la selección
+    toggleSelected(element);
+
+    // Si la butaca está seleccionada, mostrar sus datos
+    if (element.classList.contains('selected')) {
+        datos_butaca(`${id},${sala},${pelicula},${funcion},${precio}`);
+        document.getElementById('datos_butaca').style.display = 'block'; // Mostrar datos_butaca
+    } else {
+        datos_butaca(null); // Limpiar los datos si se deselecciona
+    }
+}
+
 
 document.getElementById('pelicula_select').addEventListener('change', cargar_funciones); //escucha de cambio del select de peliculas
 async function cargar_funciones(){
@@ -86,8 +96,8 @@ async function cargar_funciones(){
 
             // Crear y agregar la opción al select
             const option = document.createElement('option');
-            option.text = `${fechaFormateada} - ${funcion.hsInicio.substring(0, 5)} - $${funcion.precioBase}`;
-            option.value = `${funcion.idSala},${funcion.idFuncion},${option.text}`; //{idSala,idFuncion,funcion} datos para pasar a las butacas
+            option.text = `${fechaFormateada} - ${funcion.hsInicio.substring(0, 5)} Hs.`;
+            option.value = `${funcion.idSala},${funcion.idFuncion},${funcion.precioBase},${option.text}`; // {idSala,idFuncion,precioBase,funcion}; //{idSala,idFuncion,funcion} datos para pasar a las butacas
             $selectF.appendChild(option);
         });
 
@@ -112,20 +122,23 @@ async function datos_butaca(datos = null){ //opcion de ser nulo para que si no s
         }
         const data = await res.json();
         let sala = data.find(s => s.idSala == bpartes[1]) //obteniendo el nombre de las salas
-
+        let precio = bpartes[3];
         $div.innerHTML = 
         `
-            <h3 class="mb-3">-DATOS DE LA BUTACA-</h3>
+            <div>
             <p>N° de la butaca: <span style="color: white;">${bpartes[0]}</span></p>        <!--el span es para poner los datos en color blanco-->
             <p>Sala de la butaca: <span style="color: white;">${sala.descripcion}</span></p>
             <p>Pelicula: <span style="color: white;">${bpartes[2]}</span></p>
-            <p>Función: <span style="color: white;">${bpartes[3]}</span></p>
-            <input id="boton_reservar" type="button" class="btn btn-primary button" value="Reservar Butaca">
+            <p>Precio: <span style="color: white;">$${precio}</span></p>
+            <div class="contenedor-btn d-flex justify-content-center">
+            <input id="boton_reservar" type="button" class="btn btn-primary button" value="Reservar">
+            <input id="boton_cancelar" type="button" class="btn btn-danger button" value="Cancelar">
+            </div>
             <br><br><br><br><br><br><br>                <!--espacio para ver bien los datos, arreglar en css <3-->
-        `
+            </div>
+            `
     }
     else {
         $div.innerHTML = '';
     }
-
 }
